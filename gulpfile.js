1281173aaecad23b9c9303a295de586554a98ad0
Lollipop <trlollipopp@gmail.com>
2024-05-01 11:53:46 +0300

    created product

--- gulpfile.js
+++ gulpfile.js
@@ -45,6 +45,7 @@ function images() {
 function styles() {
   return src([
     'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css',
+    'node_modules/rateyo/src/jquery.rateyo.css',
     'app/scss/style.scss',
   ])
     .pipe(scss({ outputStyle: 'compressed' }))
@@ -61,6 +62,7 @@ function scripts() {
     'node_modules/jquery/dist/jquery.js',
     'node_modules/slick-carousel/slick/slick.js',
     'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+    'node_modules/rateyo/src/jquery.rateyo.js',
     'app/js/main.js'
   ])
     .pipe(concat('main.min.js'))
